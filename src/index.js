198de8494d6818ab285b3043dd630e8709f043af
Olech Bartlomiej <olech.bartlomiej@gmail.com>
2018-08-30 22:59:12 +0200

    Add animation to new questions
    Adding animation if app displays new questions

--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@ const userInfo={
 
 document.querySelector('#start-quiz-button').addEventListener('click', function(){
     userInfo.nick=document.querySelector('#input-nick').value;
-    document.querySelector('#his-button').checked?userInfo.category='history':userInfo.category='geography';
     document.querySelector('#page-start').setAttribute('style', 'display:none');
     document.querySelector('#page-main-content').setAttribute('style', 'display:inline-block; position:absolute; top:50%; left:50%; transform:translate(-50%, -50%)');
     if(document.querySelector('#his-button').checked){
--- src/js/checkAnswer.js
+++ src/js/checkAnswer.js
@@ -13,6 +13,8 @@ const answer1=document.querySelector('#answer-1');
 const answer2=document.querySelector('#answer-2');
 const answer3=document.querySelector('#answer-3');
 const answer4=document.querySelector('#answer-4');
+const pageMainContent= document.querySelector('#page-main-content');
+let isMarked=false;
 
 export const CheckAnswer=(categoryQuestions)=>{
   newestArray=categoryQuestions;
@@ -23,9 +25,13 @@ export const CheckAnswer=(categoryQuestions)=>{
   answer3.innerHTML=newestArray[0].answer3;
   answer4.innerHTML=newestArray[0].answer4;
 
+  isMarked?buttonNextQuestions.disabled=false:buttonNextQuestions.disabled=true;
+
   //first chooses the correct answer
     for(let i = 0; i < answers.length; i++){
       answers[i].addEventListener("click", function (){
+        isMarked=true
+        isMarked?buttonNextQuestions.disabled=false:buttonNextQuestions.disabled=true;
           for(let i = 0; i < answers.length; i++){
             if(answers[i].style.color === ""){
               answers[i].setAttribute('style', 'background:#EB8500');
@@ -39,25 +45,27 @@ export const CheckAnswer=(categoryQuestions)=>{
       } 
       //button that adds another question
       buttonNextQuestions.addEventListener('click', function(){
+        //adding animation to next question
+        pageMainContent.classList.add('pre-animation');
+        pageMainContent.classList.remove('page-main-content');
+        //disabled buttonNextQuestions
+        isMarked?buttonNextQuestions.disabled=false:buttonNextQuestions.disabled=true;
+        //changes the content of the button
+        questionNumber===9?buttonNextQuestions.innerHTML='Pokaż wynik':null;
         //clear select styles
         for(let i = 0; i < answers.length; i++){
           answers[i].setAttribute('class', '');
           answers[i].setAttribute('style', 'background:#EB8500');
         }
-
         questionNumber++;
         //checks whether the correct answer was selects 
-
-        if(elementClicked.innerHTML===newestArray[0].trueAnswer){
-          pointsAmount++;
-        }
+        elementClicked.innerHTML===newestArray[0].trueAnswer?pointsAmount++:null;
         //if questions array is empty show the result otherwise removes the first element from array
         if(questionNumber===11){
           Result(pointsAmount);
         }else{
           newestArray=newestArray.filter(e=>e.id!==idArray)
           idArray++;
-  
           question.innerHTML=newestArray[0].question;
           answer1.innerHTML=newestArray[0].answer1;
           answer2.innerHTML=newestArray[0].answer2;
@@ -65,9 +73,17 @@ export const CheckAnswer=(categoryQuestions)=>{
           answer4.innerHTML=newestArray[0].answer4;
           //updates text (number of questions)
         questionNumberH3.innerHTML=`Pytanie ${questionNumber}`;
+        //adding animation to next question
+        setTimeout(function(){
+          pageMainContent.classList.remove('pre-animation')
+          pageMainContent.classList.add('page-main-content');
+        },50)
         
         for(let i = 0; i < answers.length; i++){
           answers[i].addEventListener("click", function (){
+            //disabled buttonNextQuestions
+            isMarked=true
+            isMarked?buttonNextQuestions.disabled=false:buttonNextQuestions.disabled=true;
             for(let i = 0; i < answers.length; i++){
               if(answers[i].style.color === ""){
                 answers[i].setAttribute('style', 'background:#EB8500');
@@ -75,6 +91,7 @@ export const CheckAnswer=(categoryQuestions)=>{
                 this.setAttribute('style', 'background:#D95240; border:1px solid #ecebeb')
                 this.setAttribute('class', 'checked')
                 elementClicked=this
+                isMarked=false;
               }
             }
          });

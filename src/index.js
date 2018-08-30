a6ec1abb56893493fc14577707529ca033323eb2
Olech Bartlomiej <olech.bartlomiej@gmail.com>
2018-08-30 21:38:23 +0200

    Add a category selection
    Added conditional statement that checking which category has been chosen

--- src/index.js
+++ src/index.js
@@ -4,8 +4,7 @@ import HistoryQuestions from './history.json';
 import GeographyQuestions from './geography.json'
 
 const userInfo={
-    nick:'',
-    category:''
+    nick:''
 }
 
 document.querySelector('#start-quiz-button').addEventListener('click', function(){
@@ -13,7 +12,13 @@ document.querySelector('#start-quiz-button').addEventListener('click', function(
     document.querySelector('#his-button').checked?userInfo.category='history':userInfo.category='geography';
     document.querySelector('#page-start').setAttribute('style', 'display:none');
     document.querySelector('#page-main-content').setAttribute('style', 'display:inline-block; position:absolute; top:50%; left:50%; transform:translate(-50%, -50%)');
-    Category(HistoryQuestions)
+    if(document.querySelector('#his-button').checked){
+        Category(HistoryQuestions)
+    }else if(document.querySelector('#geo-button').checked){
+        Category(GeographyQuestions)
+    }
+    
+    
 
     
     //timer countdown

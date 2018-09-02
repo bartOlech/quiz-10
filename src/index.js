6e8033b79d5d0c40c4550564d14ab95fa0853069
Olech Bartlomiej <olech.bartlomiej@gmail.com>
2018-09-02 23:35:19 +0200

    Add a ranking section
    Added a ranking which gets a nickname and displays it in the results table

--- src/index.js
+++ src/index.js
@@ -9,6 +9,15 @@ const userInfo={
 
 document.querySelector('#start-quiz-button').addEventListener('click', function(){
     userInfo.nick=document.querySelector('#input-nick').value;
+    document.querySelector('.ranking-btn').addEventListener('click', function(){
+        document.querySelector('#page-ranking').setAttribute('style', 'display:flex')
+        document.querySelector('#page-result').setAttribute('style', 'display:none')
+        document.querySelector('#ranking-nick').innerHTML=userInfo.nick;
+    })
+    document.querySelector('#close-ranking').addEventListener('click', function(){
+        document.querySelector('#page-ranking').setAttribute('style', 'display:none')
+        document.querySelector('#page-result').setAttribute('style', 'display:flex')
+    })
     document.querySelector('#page-start').setAttribute('style', 'display:none');
     document.querySelector('#page-main-content').setAttribute('style', 'display:inline-block; position:absolute; top:50%; left:50%; transform:translate(-50%, -50%)');
     if(document.querySelector('#his-button').checked){

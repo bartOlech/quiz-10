149906cfb3a22af1cfd2577d6aa7a923ea667ab5
Olech Bartlomiej <olech.bartlomiej@gmail.com>
2018-09-08 21:15:41 +0200

    Improve the behavior of the ranking-section
    Added media queries to the ranking-cont element

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ const pageRanking= document.querySelector('#page-ranking');
 const pageResult=document.querySelector('#page-result');
 const imgLogo=document.querySelector('#img-logo');
 const inputNick=document.querySelector('#input-nick');
-const headerResult=document.querySelector('#header-result');
 const amountPoints=document.querySelector('#amount-of-points');
 const faceIco=document.querySelector('#face-ico');
 const rankingBtn=document.querySelector('.ranking-btn');
@@ -22,28 +21,30 @@ if(inputNick.value.length!=0){
     imgLogo.style.display='none';
     userInfo.nick=inputNick.value;
     rankingBtn.addEventListener('click', function(){
-    showRanking?rankingBtn.disabled=false:rankingBtn.disabled=true;
-    showRanking=true;
-    amountPoints.setAttribute('style', 'display:none')
-    faceIco.setAttribute('style', 'display:none')
-    pageRanking.setAttribute('style', 'display:flex;');
-    //document.querySelector('.wrap').setAttribute('style', 'margin-top:60%')
-    pageRanking.classList.add('pre-animation');
-    pageRanking.classList.remove('page-ranking');
-    document.querySelector('#ranking-nick').innerHTML=userInfo.nick;
-    setTimeout(function(){
-        pageRanking.classList.remove('pre-animation');
-        pageRanking.classList.add('page-ranking');
-        },50)
-    })
-
-    document.querySelector('#close-ranking').addEventListener('click', function(){
-        showRanking?rankingBtn.disabled=false:rankingBtn.disabled=true;
+    if(showRanking){
         showRanking=false;
-        imgLogo.style.display='none';
         pageRanking.setAttribute('style', 'display:none')
         amountPoints.setAttribute('style', 'display:flex')
         faceIco.setAttribute('style', 'display:flex')
+    }else{
+        showRanking=true;
+        amountPoints.setAttribute('style', 'display:none')
+        faceIco.setAttribute('style', 'display:none')
+        pageRanking.setAttribute('style', 'display:flex;');
+        pageRanking.classList.add('pre-animation');
+        pageRanking.classList.remove('page-ranking');
+        document.querySelector('#ranking-nick').innerHTML=userInfo.nick;
+        setTimeout(function(){
+            pageRanking.classList.remove('pre-animation');
+            pageRanking.classList.add('page-ranking');
+        },50)
+    }
+    })
+    document.querySelector('#close-ranking').addEventListener('click', function(){
+        showRanking=false;
+        pageRanking.setAttribute('style', 'display:none');
+        amountPoints.setAttribute('style', 'display:flex');
+        faceIco.setAttribute('style', 'display:flex');
     })
     document.querySelector('#page-start').setAttribute('style', 'display:none');
     document.querySelector('#page-main-content').setAttribute('style', 'display:inline-block;');
